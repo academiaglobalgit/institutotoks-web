c2183ee380f1ef4549ca7dc242fcfaad9e9959df
pmm01 <sistemas@pmm.com.mx>
2021-09-13 13:29:08 -0600

    13092021-5

--- php/enviarcorreo.php
+++ php/enviarcorreo.php
@@ -9,8 +9,8 @@ mysqli_set_charset($conexion,'utf8');
 
 $query = 
     "INSERT INTO escolar.tb_contacto_correos_enviados(pagina_origen, nombre, correo, telefono, ip, navegador, mensaje) 
-        VALUES ('http://institutotoks.mx/web/', '$_POST[nombre]', '$_POST[email]', '$_POST[telefono]', '$_POST[ip]', '$_POST[browser]', '$_POST[mensaje]')";
-
+        VALUES ('http://institutotoks.mx/web/', '$_POST[nombre]', '$_POST[correo]', '$_POST[telefono]', '$_POST[ip]', '$_POST[browser]', '$_POST[mensaje]')";
+$conexion->query($query);
 print_r($query);
 
 
